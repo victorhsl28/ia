91c419480902d72ee291d7a3294465b0064af207
Victor Hugo <59032636+victorhsl28@users.noreply.github.com>
2022-05-06 03:14:39 -0300

    Add files via upload

--- IA/src/metro_de_paris/MainMetroDeParis.java
+++ IA/src/metro_de_paris/MainMetroDeParis.java
@@ -34,9 +34,20 @@ public class MainMetroDeParis {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		/*for(int i = 1; i < 15; i++) {
+			for(int j = 1; j < 15; j++) {
+				System.out.println(i + " -> " + j);
+				aStar(i, j);
+				System.out.println("\n");
+			}
+		}*/
 	}
 	
 	private void aStar(int departure, int arrive) {
+		if(departure == arrive) {
+			return;
+		}
 		unread = new LinkedList<>();
 		List<Integer> read = new LinkedList<>();
 		time = 0;
@@ -59,6 +70,11 @@ public class MainMetroDeParis {
 					}
 				}
 			}
+			if(lowerStation == null) {
+				aStar(arrive, departure);
+				return;
+			}
+			
 			if(lowerStation.getId() == arrive) {
 				reading = false;
 				unread.add(lowerStation);
